f49414dbd9072fdc409e910a0c62ea612f5e85bf
marc14444 <amanijeanmarc41@gmail.com>
2024-08-04 18:19:25 +0000

    modification des profil admin

--- js/recherche.js
+++ js/recherche.js
@@ -1,6 +1,8 @@
 const url = 'http://localhost:3000/api/search/search-artisan-for-client/'
 const btnRecherche = document.getElementById('recherche');
-
+const loading = document.getElementById('loading');
+loading.style.display = "none";
+let messageEnCasDeNonTrouve = document.getElementById('messageEnCasDeNonTrouve');
 async function makeSearch(){
     const search = document.getElementById('searchBar').value;
     const adress = document.getElementById('select').value;
@@ -25,24 +27,36 @@ async function makeSearch(){
             body: JSON.stringify(dataSend)
         });
         if (!response.ok) {
+            messageEnCasDeNonTrouve.style.display = "block";
+            messageEnCasDeNonTrouve.textContent = "Oups, aucun resultat trouvé";
+            messageEnCasDeNonTrouve.style.color = "red";
+            messageEnCasDeNonTrouve.style.fontSize = "2rem";
+            messageEnCasDeNonTrouve.style.fontWeight = "bold";
             throw new Error("Erreur lors de la recherche");
         }
         
+        messageEnCasDeNonTrouve.style.display = "none";
+        document.getElementById('loading').style.display = 'flex';
+
+      // Simulate a network request or any async operation
+      setTimeout(function() {
+        document.getElementById('loading').style.display = 'none';
+      }, 2000); 
         const data = await response.json();
         console.log("recherche réussie", data);
         let html = '';
         data.searchResults.forEach(item => {
             html += `
-            <div class="bg-white rounded-lg p-4 shadow-lg">
-                <img src="${item.selfie}" alt="Profile of soumaoro inza" class="rounded-full mb-4" style="border-radius: 50%; width: 100px; height: 100px;" />
+            <div class="bg-white rounded-lg p-4 shadow-lg" style="display:grid; justify-content: center; align-items: center;">
+                <img src="${item.selfie}" alt="Profile de ${item.nomArtisan}" class="rounded-full mb-4" style="border-radius: 50%; width: 100px; height: 100px;" />
                 <h2 class="text-lg font-semibold">${item.nomArtisan}</h2>
                 <p class="text-gray-500">Metier: ${item.metier}</p>
                 <p class="text-gray-500">Lieu: ${item.local}</p>
-                <a href="tel:+225${item.telArtisan}"><button style="background-color: #FD5D14;">appel téléphonique 📞: ${item.telArtisan}</button></a>
+                <a href="tel:+225${item.telArtisan}"><button style="background-color: #FD5D14; width: 300px; height: 50px;border-radius: 10px;">appel téléphonique 📞: ${item.telArtisan}</button></a>
                 <a aria-label="Chat on WhatsApp" href="https://wa.me/225${item.telArtisan}?text=bonjour%20*${item.nomArtisan}*%20j'ai%20besoin%20de%20tes%20service">
-                <button style="background-color: #FD5D14;" class="bg-gray-200 text-gray-800 hover:bg-gray-300 mt-2 p-2 rounded">whatsapp 📞: ${item.telArtisan}</button>
-                <a />
-            </div>
+                <button style="background-color: #FD5D14; width: 300px; height: 50px; border-radius: 10px;" class="bg-gray-200 text-gray-800 hover:bg-gray-300 mt-2 p-2 rounded">whatsapp 📞: ${item.telArtisan}</button>
+                <a/>
+            </div><br/>
             `;
         });
         document.getElementById('resultSearch').innerHTML = html;
